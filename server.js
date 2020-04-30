a08988284fb87915d32f00af090eee4cf93e4297
annammattis <54290779+annammattis@users.noreply.github.com>
2020-04-30 13:03:32 -0400

    changed main

--- server.js
+++ server.js
@@ -1,37 +1,69 @@
-// Require our dependencies
-var express = require("express");
+var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var exphbs = require("express-handlebars");
+var logger = require("morgan");
 
-// Set up our port to be either the host's designated port, or 3000
-var PORT = process.env.PORT || 3000;
-
-// Instantiate our Express App
+var express = require("express");
 var app = express();
 
-// Require our routes
-var routes = require("./routes");
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
+app.use(express.static(process.cwd() + "/public"));
 
-// Connect Handlebars to our Express app
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-// Have every request go through our route middleware
-app.use(routes);
+mongoose.connect("mongodb://localhost/scraped_news");
+var db = mongoose.connection;
 
-// If deployed, use the deployed database. 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraped_news";
-
-// Connect to the Mongo DB
-mongoose.connect(MONGODB_URI);
-
-// Listen on the port
-app.listen(PORT, function() {
-  console.log("Listening on port: " + PORT);
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+    console.log("Connected to Mongoose!");
 });
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Listening on PORT " + port);
+});
+
+
+// // Require our dependencies
+// var express = require("express");
+// var mongoose = require("mongoose");
+// var exphbs = require("express-handlebars");
+
+// // Set up our port to be either the host's designated port, or 3000
+// var PORT = process.env.PORT || 3000;
+
+// // Instantiate our Express App
+// var app = express();
+
+// // Require our routes
+// var routes = require("./routes");
+
+// // Parse request body as JSON
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // Make public a static folder
+// app.use(express.static("public"));
+
+// // Connect Handlebars to our Express app
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// // Have every request go through our route middleware
+// app.use(routes);
+
+// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraped_news";
+
+// // Connect to the Mongo DB
+// mongoose.connect(MONGODB_URI);
+
+// // Listen on the port
+// app.listen(PORT, function() {
+//   console.log("Listening on port: " + PORT);
