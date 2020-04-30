46004c492548f7c21b35f60860776d2063456642
annammattis <54290779+annammattis@users.noreply.github.com>
2020-04-30 13:01:29 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var logger = require("morgan");
-
+// Require our dependencies
 var express = require("express");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+// Set up our port to be either the host's designated port, or 3000
+var PORT = process.env.PORT || 3000;
+
+// Instantiate our Express App
 var app = express();
 
-app.use(logger("dev"));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// Require our routes
+var routes = require("./routes");
 
-app.use(express.static(process.cwd() + "/public"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+// Connect Handlebars to our Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/scraped_news");
-var db = mongoose.connection;
+// Have every request go through our route middleware
+app.use(routes);
 
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-    console.log("Connected to Mongoose!");
+// If deployed, use the deployed database. 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraped_news";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
+// Listen on the port
+app.listen(PORT, function() {
+  console.log("Listening on port: " + PORT);
 });
-
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log("Listening on PORT " + port);
-});
-
